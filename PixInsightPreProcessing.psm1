a85ff3f8711afb91ec502fe39ebcf59092ef7ea2
Jonathan W. MacCollum <Jonathan.MacCollum@gmail.com>
2020-03-09 23:12:20 -0400

    Sorting of Incoming Data

--- PixInsightPreProcessing.psm1
+++ PixInsightPreProcessing.psm1
@@ -81,10 +81,20 @@ save integration -p=$output --nodialog --nomessages --noverify"
     $ScriptToRun = Rename-Item ($ScriptToRun.FullName) ($ScriptToRun.FullName+".scp") -PassThru
     $Template|Out-File $ScriptToRun -Force
     try {
+        Write-Debug "Invoking Script $(Get-Content $Path)"
         Invoke-PixInsightScript `
         -PixInsightSlot $PixInsightSlot `
         -Path $ScriptToRun `
         -KeepOpen:$KeepOpen
+
+        $OutputFile.Refresh()
+        if($OutputFile.Exists) {
+            Write-Host "Integration image saved successfully to $($OutputFile.FullName)"
+        }
+        else{
+            Write-Error "Pixinsight exited without producing expected file."
+
+        }
     }
     finally {
         Remove-Item $ScriptToRun -Force
@@ -367,3 +377,182 @@ P.noGUIMessages = true;
         Remove-Item $executionScript -Force
     }
 } 
+
+Function Invoke-DarkFlatFrameSorting
+{
+    [CmdletBinding()]
+    param (
+        [System.IO.DirectoryInfo]$DropoffLocation,
+        [System.IO.DirectoryInfo]$ArchiveDirectory,
+        [int]$PixInsightSlot
+    )
+
+    Get-ChildItem $DropoffLocation "*.xisf" |
+    foreach-object { Get-XisfFitsStats -Path $_ } |
+    where-object ImageType -eq "DARKFLAT" |
+    group-object Filter,FocalLength |
+    foreach-object {
+        $images=$_.Group
+        $filter=$images[0].Filter.Trim()
+        $focalLength=($images[0].FocalLength).TrimEnd('mm')+'mm'
+        $flatDate = ([DateTime]$images[0].LocalDate).Date.ToString('yyyyMMdd')
+        $targetDirectory = "$ArchiveDirectory\$focalLength\Flats"
+        $masterDark = "$targetDirectory\$($flatDate).MasterDarkFlat.$($filter).xisf"
+        if(-not (Test-Path $masterDark)) {
+            Write-Host "Integrating $($images.Count) Dark Flats for $filter at $focalLength"
+            $darkFlats = $images | foreach-object {$_.Path}
+            Invoke-PiDarkIntegration `
+                -Images $darkFlats `
+                -PixInsightSlot $PixInsightSlot `
+                -OutputFile $masterDark
+        }
+        $images | Foreach-Object {Move-Item -Path $_.Path -Destination "$targetDirectory\$flatDate"}
+    }    
+}
+
+Function Invoke-FlatFrameSorting
+{
+    [CmdletBinding()]
+    param (
+        [System.IO.DirectoryInfo]$DropoffLocation,
+        [System.IO.DirectoryInfo]$ArchiveDirectory,
+        [System.IO.DirectoryInfo]$CalibratedFlatsOutput,
+        [int]$PixInsightSlot
+    )
+    Get-ChildItem $DropoffLocation "*.xisf" |
+    foreach-object { Get-XisfFitsStats -Path $_ } |
+    where-object ImageType -eq "FLAT" |
+    group-object Filter,FocalLength |
+    foreach-object {
+        $flats=$_.Group
+        $filter=$flats[0].Filter.Trim()
+        $focalLength=($flats[0].FocalLength).TrimEnd('mm')+'mm'
+        $flatDate = ([DateTime]$flats[0].LocalDate).Date.ToString('yyyyMMdd')
+
+        Write-Host "Processing $($flats.Count) Flats for filter '$filter' at $focalLength"
+
+        $targetDirectory = "$ArchiveDirectory\$focalLength\Flats"
+        $masterDark = "$targetDirectory\$flatDate.MasterDarkFlat.$filter.xisf"
+        if(Test-Path $masterDark) {
+            $masterCalibratedFlat = "$targetDirectory\$flatDate.MasterFlatCal.$filter.xisf"
+            if(-not (Test-Path $masterCalibratedFlat)) {
+                $flats | foreach-object{
+                    $input = $_.Path
+                    $output = Join-Path ($CalibratedFlatsOutput.FullName) ($input.Name.Replace($input.Extension,"")+"_c.xisf")
+                    Add-Member -InputObject $_ -Name "CalibratedFlat" -MemberType NoteProperty -Value $output -Force
+                }
+                $toCalibrate = $flats | where-object {-not (Test-Path $_.CalibratedFlat)} | foreach-object {$_.Path}
+                if($toCalibrate){
+                    Invoke-PiFlatCalibration `
+                        -Images $toCalibrate `
+                        -MasterDark $masterDark `
+                        -OutputPath $CalibratedFlatsOutput `
+                        -PixInsightSlot $PixInsightSlot
+                }
+                $calibratedFlats = $flats|foreach-object {Get-Item $_.CalibratedFlat}
+                if($flats) {
+                    Invoke-PiFlatIntegration `
+                        -Images $calibratedFlats `
+                        -PixInsightSlot $PixInsightSlot `
+                        -OutputFile $masterCalibratedFlat
+                }
+            }
+        }
+
+        $masterNoCalFlat = "$targetDirectory\$FlatDate.MasterFlatNoCal.$($filter).xisf"
+        $toIntegrate = $flats|foreach-object {$_.Path}
+        if(-not (Test-Path $masterNoCalFlat)) {
+            Invoke-PiFlatIntegration `
+                -Images $toIntegrate `
+                -PixInsightSlot $PixInsightSlot `
+                -OutputFile $masterNoCalFlat
+        }
+        $flats | Foreach-Object {Move-Item -Path $_.Path -Destination "$targetDirectory\$flatDate"}
+    }
+}
+
+
+Function Get-CalibrationFile
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][System.IO.FileInfo]$Path,
+        [Parameter(Mandatory)][System.IO.DirectoryInfo]$CalibratedPath,
+        [Parameter()][System.IO.DirectoryInfo[]]$AdditionalSearchPaths
+    )
+    $calibratedFileName = $Path.Name.Substring($Path.Name.Length-$input.Extension.Length)+"_c.xisf"
+    $calibrated = Join-Path ($CalibratedPath.FullName) ($calibratedFileName)
+    if(-not (test-path $calibrated) -and ($AdditionalSearchPaths)){
+        $calibrated = $AdditionalSearchPaths | 
+            foreach-object { join-path $_ $calibratedFileName } |
+            where-object { test-path $_ } | 
+            select-object -first 1
+    }
+    $calibrated
+}
+
+Function Invoke-LightFrameSorting
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][System.IO.DirectoryInfo]$DropoffLocation,
+        [Parameter(Mandatory)][System.IO.DirectoryInfo]$ArchiveDirectory,
+
+        [Parameter(Mandatory)][String]$Filter,
+        [Parameter(Mandatory)][int]$FocalLength,
+        [Parameter(Mandatory)][int]$Exposure,
+        [Parameter(Mandatory)][int]$Gain,
+        [Parameter(Mandatory)][int]$Offset,
+
+        [Parameter(Mandatory)][System.IO.FileInfo]$MasterDark,
+        [Parameter(Mandatory)][System.IO.FileInfo]$MasterFlat,
+        [Parameter(Mandatory)][System.IO.DirectoryInfo]$OutputPath,
+
+        [Parameter(Mandatory)][int]$PixInsightSlot,
+        [System.IO.DirectoryInfo[]]$AdditionalSearchPaths
+    )
+
+    Get-ChildItem $DropoffLocation "*.xisf" |
+    foreach-object { Get-XisfFitsStats -Path $_ } |
+    where-object ImageType -eq "Light" |
+    where-object Filter -eq $Filter |
+    where-object Exposure -eq $Exposure |
+    where-object CCDGain -eq $CCDGain |
+    where-object Offset -eq $Offset |
+    where-object FocalLength -eq $FocalLength |
+    Group-Object Object |
+    foreach-object {
+        $object = $_.Name
+
+        $toCalibrate = $_.Group |
+            foreach-object {
+                $x = $_
+                $calibrated = Get-CalibrationFile -Path $_.Path -CalibratedPath $OutputPath -AdditionalSearchPaths $AdditionalSearchPaths
+                Add-Member -InputObject $x -Name "Calibrated" -MemberType NoteProperty -Value $calibrated -Force
+                $x
+            } |
+            where-object {-not (Test-Path $_.Calibrated)} | foreach-object {$_.Path}
+        if($toCalibrate){
+            Write-Host "Calibrating $($toCalibrate.Count) Light Frames for target $object"
+            Invoke-PiLightCalibration `
+                -Images $toCalibrate `
+                -MasterDark $MasterDark `
+                -MasterFlat $MasterFlat `
+                -OutputPath $OutputPath `
+                -PixInsightSlot $PixInsightSlot
+        }
+
+        $objectNameParts = $object.Split(' ')
+        $archive = Join-Path $ArchiveDirectory "$($FocalLength)mm" -AdditionalChildPath $object
+        if(-not (test-path $archive)){
+            $archive = Join-Path $ArchiveDirectory "$($FocalLength)mm" -AdditionalChildPath ($objectNameParts[0] + ' '+ $objectNameParts[1])
+        }
+        if(-not (test-path $archive)){
+            $archive = Join-Path $ArchiveDirectory "$($FocalLength)mm" -AdditionalChildPath ($objectNameParts[0])
+        }
+        $archive = Join-Path $archive "$Exposure.00"
+        [System.IO.Directory]::CreateDirectory($archive) >> $null
+
+        $_.Group | Foreach-Object {Move-Item -Path $_.Path -Destination $archive}
+    }
+}
--- PreProcessing.ps1
+++ PreProcessing.ps1
@@ -2,186 +2,95 @@ import-module $PSScriptRoot/pixinsightpreprocessing.psm1 -Force
 import-module $PSScriptRoot/PsXisfReader.psm1
 
 
-$PixInsightSlot=200
 $DropoffLocation = "D:\Backups\Camera\Dropoff\NINA"
 $ArchiveDirectory="D:\Backups\Camera\Astrophotography"
-$CalibratedFlatsOutput=Get-Item "S:\PixInsight\CalibratedFlats"
-$CalibratedLightsOutput=Get-Item "S:\PixInsight\Calibrated"
-
-$DarkLibrary = (Get-ChildItem D:\Backups\Camera\ASI071mc-pro "*Dark*.xisf")
-$DarkLibrary += (Get-ChildItem 'D:\Backups\Camera\2019\Dark Library' "*.xisf")
-$DarkLibrary = $DarkLibrary | Foreach-Object {Get-XisfFitsStats -Path $_ }
-
-$FilesToProcess=
-    Get-ChildItem $DropoffLocation "*.xisf" |
-    foreach-object { Get-XisfFitsStats -Path $_ } |
-    group-object ImageType
-
-($FilesToProcess|where-object Name -eq "DARKFLAT").Group |
-    group-object Filter,FocalLength |
-    foreach-object {
-        $images=$_.Group
-        $filter=$images[0].Filter.Trim()
-        $focalLength=($images[0].FocalLength).TrimEnd('mm')+'mm'
-        $flatDate = ([DateTime]$images[0].LocalDate).Date.ToString('yyyyMMdd')
-
-        Write-Host "Processing Dark Flats for $filter at $focalLength"
-
-        $targetDirectory = "$ArchiveDirectory\$focalLength\Flats"
-        $masterDark = "$targetDirectory\$($flatDate).MasterDarkFlat.$($filter).xisf"
-        if(-not (Test-Path $masterDark)) {
-            $darkFlats = $images | foreach-object {$_.Path}
-            Invoke-PiDarkIntegration `
-                -Images $darkFlats `
-                -PixInsightSlot $PixInsightSlot `
-                -OutputFile $masterDark `
-                -Verbose
-        }
-        $images | Foreach-Object {Move-Item -Path $_.Path -Destination "$targetDirectory\$flatDate"}
-    }
-
-Function Add-Calibrated
-{
-    [CmdletBinding()]
-    param (
-        [Parameter()]$File,
-        [Parameter()][System.IO.DirectoryInfo]$CalibratedPath
-    )
-    $input = $File.Path
-    $calibrated = Join-Path ($CalibratedPath.FullName) ($input.Name.Replace($input.Extension,"")+"_c.xisf")
-    Add-Member -InputObject $_ -Name "Calibrated" -MemberType NoteProperty -Value $calibrated -Force
-    $File
-}
-
-($FilesToProcess|where-object Name -eq "FLAT").Group |
-    group-object Filter,FocalLength |
-    foreach-object {
-        $flats=$_.Group
-        $filter=$flats[0].Filter.Trim()
-        $focalLength=($flats[0].FocalLength).TrimEnd('mm')+'mm'
-        $flatDate = ([DateTime]$flats[0].LocalDate).Date.ToString('yyyyMMdd')
-
-        Write-Host "Processing Flats for $filter at $focalLength"
-
-        $targetDirectory = "$ArchiveDirectory\$focalLength\Flats"
-        $masterDark = "$targetDirectory\$flatDate.MasterDarkFlat.$filter.xisf"
-        if(Test-Path $masterDark) {
-            $masterCalibratedFlat = "$targetDirectory\$flatDate.MasterFlatCal.$filter.xisf"
-            if(-not (Test-Path $masterCalibratedFlat)) {
-                $flats | foreach-object{
-                    $input = $_.Path
-                    $output = Join-Path ($CalibratedFlatsOutput.FullName) ($input.Name.Replace($input.Extension,"")+"_c.xisf")
-                    Add-Member -InputObject $_ -Name "CalibratedFlat" -MemberType NoteProperty -Value $output -Force
-                }
-                $toCalibrate = $flats | where-object {-not (Test-Path $_.CalibratedFlat)} | foreach-object {$_.Path}
-                if($toCalibrate){
-                    Invoke-PiFlatCalibration `
-                        -Images $toCalibrate `
-                        -MasterDark $masterDark `
-                        -OutputPath $CalibratedFlatsOutput `
-                        -PixInsightSlot $PixInsightSlot -Verbose
-                }
-                $calibratedFlats = $flats|foreach-object {Get-Item $_.CalibratedFlat}
-                if($flats) {
-                    Invoke-PiFlatIntegration `
-                        -Images $calibratedFlats `
-                        -PixInsightSlot $PixInsightSlot `
-                        -OutputFile $masterCalibratedFlat `
-                        -Verbose
-                }
-            }
-        }
-
-        $masterNoCalFlat = "$TargetDirectory\$FlatDate.MasterFlatNoCal.$($filter).xisf"
-        $toIntegrate = $flats|foreach-object {$_.Path}
-        if(-not (Test-Path $masterNoCalFlat)) {
-            Invoke-PiFlatIntegration `
-                -Images $toIntegrate `
-                -PixInsightSlot $PixInsightSlot `
-                -OutputFile $masterNoCalFlat `
-                -Verbose
-        }
-        $flats | Foreach-Object {Move-Item -Path $_.Path -Destination "$targetDirectory\$flatDate"}
-    }
-
-Function Invoke-LightFrameSorting
-{
-    [CmdletBinding()]
-    param (
-        [Object[]]$Lights,
-        [String]$Filter,
-        [int]$FocalLength,
-        [int]$Exposure,
-        [System.IO.FileInfo]$MasterDark,
-        [System.IO.FileInfo]$MasterFlat,
-        [System.IO.DirectoryInfo]$OutputPath,
-        [int]$PixInsightSlot
-    )
-
-    $Lights | 
-        where-object Filter -eq $Filter |
-        where-object Exposure -eq $Exposure |
-        where-object FocalLength -eq $FocalLength |
-        Group-Object Object |
-        foreach-object {
-            $object = $_.Name
-
-            $toCalibrate = $_.Group |
-                foreach-object {Add-Calibrated -File $_ -CalibratedPath $CalibratedLightsOutput } |
-                where-object {-not (Test-Path $_.Calibrated)} | foreach-object {$_.Path}
-            if($toCalibrate){
-                Invoke-PiLightCalibration `
-                    -Images $toCalibrate `
-                    -MasterDark $MasterDark `
-                    -MasterFlat $MasterFlat `
-                    -OutputPath $CalibratedLightsOutput `
-                    -PixInsightSlot $PixInsightSlot -Verbose
-            }
-
-            $objectNameParts = $object.Split(' ')
-            $archive = Join-Path $ArchiveDirectory "$($FocalLength)mm" -AdditionalChildPath $object
-            if(-not (test-path $archive)){
-                $archive = Join-Path $ArchiveDirectory "$($FocalLength)mm" -AdditionalChildPath ($objectNameParts[0] + ' '+ $objectNameParts[1])
-            }
-            if(-not (test-path $archive)){
-                $archive = Join-Path $ArchiveDirectory "$($FocalLength)mm" -AdditionalChildPath ($objectNameParts[0])
-            }
-            $archive = Join-Path $archive "$Exposure.00"
-            [System.IO.Directory]::CreateDirectory($archive) >> $null
-
-            $_.Group | Foreach-Object {Move-Item -Path $_.Path -Destination $archive}
-        }
-}
-
-
+Invoke-DarkFlatFrameSorting `
+    -DropoffLocation $DropoffLocation `
+    -ArchiveDirectory $ArchiveDirectory `
+    -PixInsightSlot 200
+Invoke-FlatFrameSorting `
+    -DropoffLocation $DropoffLocation `
+    -ArchiveDirectory $ArchiveDirectory `
+    -CalibratedFlatsOutput "S:\PixInsight\CalibratedFlats" `
+    -PixInsightSlot 200
 Invoke-LightFrameSorting `
-    -Lights (($FilesToProcess|where-object Name -eq "Light").Group) `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
     -MasterDark "D:\Backups\Camera\ASI071mc-pro\MasterDark.NINA.240s.-15C_x20.Gain90Offset65.NoCalibration.xisf" `
     -MasterFlat "D:\Backups\Camera\2019\20191130 - 135mm 071mc L3\MasterFlat.L3.xisf" `
-    -Filter "L3" `
-    -FocalLength 135 `
-    -Exposure 240 `
-    -OutputPath $CalibratedLightsOutput `
-    -PixInsightSlot $PixInsightSlot `
-    -Verbose
-
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -Filter "L3" -FocalLength 135 -Exposure 240 -Gain 90 -Offset 65 `
+    -PixInsightSlot 200
 Invoke-LightFrameSorting `
-    -Lights (($FilesToProcess|where-object Name -eq "Light").Group) `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
     -MasterDark "D:\Backups\Camera\2019\Dark Library\20190313.60x120s.masterdark.-10c.90g.65o.nobias.xisf" `
     -MasterFlat "D:\Backups\Camera\2019\20191130 - 135mm 071mc L3\MasterFlat.L3.xisf" `
-    -Filter "L3" `
-    -FocalLength 135 `
-    -Exposure 120 `
-    -OutputPath $CalibratedLightsOutput `
-    -PixInsightSlot $PixInsightSlot `
-    -Verbose
+    -Filter "L3" -FocalLength 135 -Exposure 120 -Gain 90 -Offset 65 `
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -PixInsightSlot 200
 Invoke-LightFrameSorting `
-    -Lights (($FilesToProcess|where-object Name -eq "Light").Group) `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
     -MasterDark "D:\Backups\Camera\2019\Dark Library\20190313.60x30s.masterdark.-10c.90g.65o.nobias.integration.xisf" `
     -MasterFlat "D:\Backups\Camera\2019\20191130 - 135mm 071mc L3\MasterFlat.L3.xisf" `
-    -Filter "L3" `
-    -FocalLength 135 `
-    -Exposure 30 `
-    -OutputPath $CalibratedLightsOutput `
-    -PixInsightSlot $PixInsightSlot `
-    -Verbose
+    -Filter "L3" -FocalLength 135 -Exposure 30 -Gain 90 -Offset 65 `
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -PixInsightSlot 200
+
+
+Invoke-LightFrameSorting `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+    -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20190909\25x60s.-15C.G111.O8.masterdark.nocalibration.xisf" `
+    -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.R.xisf" `
+    -Filter "R" -FocalLength 1000 -Exposure 60 -Gain 111 -Offset 8 `
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -PixInsightSlot 200
+Invoke-LightFrameSorting `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+    -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20190909\25x60s.-15C.G111.O8.masterdark.nocalibration.xisf" `
+    -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.G.xisf" `
+    -Filter "G" -FocalLength 1000 -Exposure 60 -Gain 111 -Offset 8  `
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -PixInsightSlot 200
+Invoke-LightFrameSorting `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+    -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20190909\25x60s.-15C.G111.O8.masterdark.nocalibration.xisf" `
+    -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.B.xisf" `
+    -Filter "B" -FocalLength 1000 -Exposure 60 -Gain 111 -Offset 8  `
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -PixInsightSlot 200
+Invoke-LightFrameSorting `
+    -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+    -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20190525\60x30s.-10C.G111.O8.masterdark.nocalibration.xisf" `
+    -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.L.xisf" `
+    -Filter "L" -FocalLength 1000 -Exposure 30 -Gain 111 -Offset 8  `
+    -OutputPath "S:\PixInsight\Calibrated" `
+    -PixInsightSlot 200
+
+$('Ha','L','R','G','B')|foreach-object {
+    Invoke-LightFrameSorting `
+        -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+        -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20191220\30x240s.-15C.G111.O8.masterdark.nocalibration.xisf" `
+        -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.$_.xisf" `
+        -Filter $_ -FocalLength 1000 -Exposure 240 -Gain 111 -Offset 8  `
+        -OutputPath "S:\PixInsight\Calibrated" `
+        -PixInsightSlot 200
+}
+
+$('Ha','L','R','G','B')|foreach-object {
+    Invoke-LightFrameSorting `
+        -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+        -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20191220\30x120s.-15C.G111.O8.masterdark.nocalibration.xisf" `
+        -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.$_.xisf" `
+        -Filter $_ -FocalLength 1000 -Exposure 120 -Gain 111 -Offset 8  `
+        -OutputPath "S:\PixInsight\Calibrated" `
+        -PixInsightSlot 200
+}
+
+$('Ha','Oiii','Sii')|foreach-object {
+    Invoke-LightFrameSorting `
+        -DropoffLocation $DropoffLocation -ArchiveDirectory $ArchiveDirectory `
+        -MasterDark "D:\Backups\Camera\ASI183mm-Pro\20191220\74x600s.-15C.G53.O10.masterdark.nocalibration.xisf" `
+        -MasterFlat "D:\Backups\Camera\Astrophotography\1000mm\Flats\20200306.MasterFlatCal.$_.xisf" `
+        -Filter $_ -FocalLength 1000 -Exposure 600 -Gain 53 -Offset 10  `
+        -OutputPath "S:\PixInsight\Calibrated" `
+        -PixInsightSlot 200
+}
