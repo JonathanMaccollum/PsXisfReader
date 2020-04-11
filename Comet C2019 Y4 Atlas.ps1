85e841c9bd0ae8cae694c9aa78e3a3e0f8d7775a
Jonathan W. MacCollum <Jonathan.MacCollum@gmail.com>
2020-04-11 11:34:32 -0400

    Comet C2019 Y4 Atlas Preprocessing

--- Comet C2019 Y4 Atlas.ps1
+++ Comet C2019 Y4 Atlas.ps1
@@ -5,7 +5,7 @@ $target="E:\Astrophotography\1000mm\Comet C2019 Y4 Atlas"
 $CalibrationPath = "E:\PixInsightLT\Calibrated"
 $WeightedOutputPath = "S:\PixInsight\Weighted"
 $AlignedOutputPath = "S:\PixInsight\Aligned"
-$BackupCalibrationPaths = @("T:\PixInsightLT\Calibrated","N:\PixInsightLT\Calibrated")
+$BackupCalibrationPaths = @("T:\PixInsightLT\Calibrated")
 
 $data = 
     Get-XisfLightFrames -Path $target -Recurse |
@@ -20,6 +20,8 @@ $data =
         Add-Member -InputObject $x -Name "Calibrated" -MemberType NoteProperty -Value $y -Force
         $x
     }
+
+
 $uncalibrated = $data | where-object {-not $_.Calibrated}
 if($uncalibrated){
     $uncalibrated|foreach-object {
@@ -31,94 +33,72 @@ if($uncalibrated){
     }
 }
 
+$data|group-object ObsDateMinus12hr,Filter|foreach-object {
+    $images = $_.Group
+    $filter=$images[0].Filter.Trim()
+    $obsDate=$images[0].ObsDateMinus12hr
+    $toWeigh = $_.Group.Calibrated
 
-$calibrated = $data |
-    where-object { $uncalibrated -notcontains $_ } |
-    group-object Filter,Exposure |
-    foreach-object {
-        $images = $_.Group
-        $filter=$images[0].Filter.Trim()
-        $exposure=$images[0].Exposure.Trim()
-        $toWeigh = $_.Group.Calibrated
+    $resultCsv="$target\Subframe.$($obsDate.ToString('yyyyMMdd')).$filter.csv"
+    $resultData="$target\Subframe.$($obsDate.ToString('yyyyMMdd')).$filter.Data.csv"
 
-        $resultCsv="$target\Subframe.$filter.$exposure.csv"
-        $resultData="$target\Subframe.$filter.$exposure.Data.csv"
-        if(-not (test-path $resultCsv)) {
-            Start-PiSubframeSelectorWeighting `
-            -PixInsightSlot 200 `
-            -OutputPath $WeightedOutputPath `
-            -Images $toWeigh `
-            -WeightingExpression "(15*(1-(FWHM-FWHMMin)/(FWHMMax-FWHMMin))
-            + 25*(1-(Eccentricity-EccentricityMin)/(EccentricityMax-EccentricityMin))
-            + 15*(SNRWeight-SNRWeightMin)/(SNRWeightMax-SNRWeightMin)
-            + 20*(1-(Median-MedianMin)/(MedianMax-MedianMin))
-            + 10*(Stars-StarsMin)/(StarsMax-StarsMin))
-            + 30"
+    if(-not (test-path $resultCsv)) {
+        set-clipboard -Value $resultCsv
+        write-host "Starting Subframe Selector... Save the resulting output CSV file to $resultCsv before exiting."
+        Start-PiSubframeSelectorWeighting `
+        -PixInsightSlot 200 `
+        -OutputPath $WeightedOutputPath `
+        -Images $toWeigh `
+        -WeightingExpression "(10*(1-(FWHM-FWHMMin)/(FWHMMax-FWHMMin))
+        + 10*(1-(Eccentricity-EccentricityMin)/(EccentricityMax-EccentricityMin))
+        + 10*(SNRWeight-SNRWeightMin)/(SNRWeightMax-SNRWeightMin)
+        + 20*(1-(Median-MedianMin)/(MedianMax-MedianMin))
+        + 30*(Stars-StarsMin)/(StarsMax-StarsMin))
+        + 30" `
+        -ApprovalExpression "Stars > 25 && NoiseRatio > 0.94 && Median < 75"
+    }    
+    $subframeResults = Get-Content -Path $resultCsv
+    $subframeResults | Select-Object -Skip 29|Out-File -Path $resultData -Force
+    $subframeResults | Select-Object -First 29
+    $subframeData    = Import-Csv -Path "$resultData"|sort-object {[double]$_.Weight} -Descending
+    $subframeData|Format-Table
+}
+
+
+$data|group-object ObsDateMinus12hr|foreach-object {
+    $images = $_.Group
+    $obsDate=$images[0].ObsDateMinus12hr
+
+    $all = Get-ChildItem -Path $target "Subframe.$($obsDate.ToString('yyyyMMdd')).*.Data.csv" | 
+        Import-CSV |
+        foreach-object {
+            $x=$_
+            $y = Get-XisfFitsStats -Path ($x.File.Replace("/","\"))
+            $w=$null
+            if($x.Approved -eq "true") {
+                $w = Get-Item (join-path $WeightedOutputPath ($y.Path.Name.TrimEnd(".xisf")+"_a.xisf"))
+                $r = join-path $AlignedOutputPath ($y.Path.Name.TrimEnd(".xisf")+"_a_r.xisf")
+            }
+            Add-Member -InputObject $y -Name "Approved" -MemberType NoteProperty -Value ([bool]::Parse($x.Approved))
+            Add-Member -InputObject $y -Name "Weight" -MemberType NoteProperty -Value ([decimal]::Parse($x.Weight))
+            Add-Member -InputObject $y -Name "Weighted" -MemberType NoteProperty -Value ($w)
+            Add-Member -InputObject $y -Name "Aligned" -MemberType NoteProperty -Value ($r)
+            $y
         }
+    $referenceFrame = $all|sort-object {[double]$_.Weight} -Descending | select-object -First 1
         
-        $subframeResults = Get-Content -Path $resultCsv
-        $subframeResults | Select-Object -Skip 29|Out-File -Path $resultData -Force
-        $subframeResults | Select-Object -First 29
-        $subframeData    = Import-Csv -Path "$resultData"|sort-object {[double]$_.Weight} -Descending
-        $subframeData|FT
-    }
+    $toAlign = $all | where-object {-not (Test-Path $_.Aligned) -and ($_.Weighted) } | foreach-object {$_.Weighted}
+    write-host "Aligning $($toAlign.Count) for night $obsDate"
+    $x = @{
+        PixInsightSlot = 200
+        Images = $toAlign
+        ReferencePath = ($referenceFrame.Weighted)
+        OutputPath = $AlignedOutputPath
+    }    
+    Invoke-PiStarAlignment @x    
+}
 
-    $stats = get-childitem $target *.data.csv |
-    import-csv |
-    foreach-object {
-        $x=$_
-        $y = Get-XisfFitsStats -Path ($x.File.Replace("/","\"))
-        $w=$null
-        if($x.Approved -eq "true") {
-            $w = Get-Item (join-path $WeightedOutputPath ($y.Path.Name.TrimEnd(".xisf")+"_a.xisf"))
-        }
-        Add-Member -InputObject $y -Name "Approved" -MemberType NoteProperty -Value ([bool]::Parse($x.Approved))
-        Add-Member -InputObject $y -Name "Weight" -MemberType NoteProperty -Value ([decimal]::Parse($x.Weight))
-        Add-Member -InputObject $y -Name "Weighted" -MemberType NoteProperty -Value ($w)
-        $y
-    }
-$summary = $stats |
-group-object Approved,Filter,Exposure |
-foreach-object {
-    $approved=$_.Values[0]
-    $filter=$_.Values[1]
-    $exposure=$_.Values[2]
-    $topWeight = $_.Group | sort-object Weight -Descending | select-object -first 1
-    new-object psobject -Property @{
-        Approved=$approved
-        Filter=$filter
-        Exposure=$exposure
-        Exposures=$_.Group.Count
-        ExposureTime=([TimeSpan]::FromSeconds($_.Group.Count*$exposure))
-        Images=$_.Group
-        TopWeight = $topWeight.Path
-    } } 
-$summary |
-    Sort-Object Approved,Filter |
-    Format-Table Approved,Filter,Exposures,Exposure,ExposureTime,TopWeight
 
-write-host "Rejected:"
-[TimeSpan]::FromSeconds((
-$summary|where-Object Approved -eq $false | foreach-object {$_.ExposureTime.TotalSeconds} |Measure-Object  -Sum).Sum).ToString()
-write-host "Approved:"
-[TimeSpan]::FromSeconds((
-    $summary|where-Object Approved -eq $true|foreach-object {$_.ExposureTime.TotalSeconds} |Measure-Object  -Sum).Sum).ToString()
-
-$referenceFrame = $stats | where-object Approved -eq "true" | sort-object "Weight" -Descending | select-object -first 1
-
-<#
-$stats | where-object Approved -eq "true" | group-object Filter | foreach-object {
-$filter = $_.Group[0].Filter
-
-write-host "Aligning $($_.Group.Count) for filter $filter"
-$images = $_.Group | foreach-object {$_.Weighted}
-Invoke-PiStarAlignment `
-    -PixInsightSlot 200 `
-    -Images $images `
-    -ReferencePath ($referenceFrame.Weighted) `
-    -OutputPath $AlignedOutputPath `
-    -KeepOpen
-}#>
 
 $aligned = Get-XisfFile -Path $AlignedOutputPath |
     where-object object -like "'Comet*Y4*" 
