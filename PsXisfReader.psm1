d82587dff382aaee1a3d351134f23199077245b1
Jonathan W. MacCollum <Jonathan.MacCollum@gmail.com>
2020-04-25 20:48:06 -0400

    Fix: Pipeline support Fits: History/Bugfixes

--- PsXisfReader.psm1
+++ PsXisfReader.psm1
@@ -39,58 +39,65 @@ Function Get-XisfFitsStats
         [Parameter(ValueFromPipeline=$true,Mandatory=$true)][System.IO.FileInfo]$Path,
         [Parameter(ValueFromPipeline=$false)][Hashtable]$Cache
     )
-    if($Cache -and $Cache.ContainsKey($Path.FullName))
-    {
-        Write-Output ($Cache[$Path.FullName])
-    }
-    else
-    {
-        $stream = [System.IO.File]::OpenRead($Path.FullName)
-        $reader=new-object System.IO.BinaryReader $stream
-        try
+    process{            
+        if($Cache -and $Cache.ContainsKey($Path.FullName))
         {
-
-            Read-XisfSignature $reader > $null
-            $header=Read-XisfHeader $reader
-            $fits = $header.xisf.Image.FITSKeyword
-            $filter = $fits.Where({$_.Name -eq 'FILTER'}).value
-            if($filter)
-            {
-                $filter = $filter.Replace('5nm','').Replace('3nm','')
-            }
-            $result = new-object psobject -Property @{
-                Exposure=$fits.Where{$_.Name -eq 'EXPOSURE'}.value
-                Filter=$filter
-                Instrument=$fits.Where{$_.Name -eq 'INSTRUME'}.value
-                Object=$fits.Where{$_.Name -eq 'OBJECT'}.value
-                Gain=$fits.Where{$_.Name -eq 'GAIN'}.value
-                Offset=$fits.Where{$_.Name -eq 'OFFSET'}.value
-                ImageType=$fits.Where{$_.Name -eq 'IMAGETYP'}.value
-                CCDTemp=$fits.Where{$_.Name -eq 'CCD-TEMP'}.value
-                SetTemp=$fits.Where{$_.Name -eq 'SET-TEMP'}.value
-                FocalLength=$fits.Where{$_.Name -eq 'FOCALLEN'}.value
-                FocalRatio=$fits.Where{$_.Name -eq 'FOCRATIO'}.value
-                ObsDate=$fits.Where{$_.Name -eq 'DATE-OBS'}.value
-                ObsDateMinus12hr=([DateTime]($fits.Where{$_.Name -eq 'DATE-OBS'}.value.Trim("'"))).AddHours(-12).Date
-                LocalDate=$fits.Where{$_.Name -eq 'DATE-LOC'}.value
-                SSWeight=$fits.Where{$_.Name -eq 'SSWEIGHT'}.value
-                Pedestal=$fits.Where{$_.Name -eq 'PEDESTAL'}.value
-                Path=$Path
-            }
-            if($Cache)
-            {
-                $Cache.Add($Path.FullName,$result)
-            }
-            Write-Output $result
+            Write-Output ($Cache[$Path.FullName])
         }
-        catch [System.Xml.XmlException]{
-            Write-Warning ("An error occured reading the file "+($Path.FullName))
-            Write-Verbose $_.Exception.ToString()
-        }
-        finally
+        else
         {
-            $reader.Dispose()
-            $stream.Dispose()
+            $stream = [System.IO.File]::OpenRead($Path.FullName)
+            $reader=new-object System.IO.BinaryReader $stream
+            try
+            {
+                Read-XisfSignature $reader > $null
+                $header=Read-XisfHeader $reader
+                $fits = $header.xisf.Image.FITSKeyword
+                $filter = $fits.Where({$_.Name -eq 'FILTER'}).value
+                if($filter)
+                {
+                    $filter = $filter.Replace('5nm','').Replace('3nm','')
+                }
+                $obsDate = $fits.Where{$_.Name -eq 'DATE-OBS'}.value
+                $obsDateMinus12hr= $null
+                if($obsDate){
+                    $obsDateMinus12hr=$obsDate|%{([DateTime]($_.Trim("'"))).AddHours(-12).Date}
+                }
+                $result = new-object psobject -Property @{
+                    Exposure=$fits.Where{$_.Name -eq 'EXPOSURE'}.value
+                    Filter=$filter
+                    Instrument=$fits.Where{$_.Name -eq 'INSTRUME'}.value|%{if($_){$_.Trim("'")}}
+                    Object=$fits.Where{$_.Name -eq 'OBJECT'}.value|%{if($_){$_.Trim("'")}}
+                    Gain=$fits.Where{$_.Name -eq 'GAIN'}.value
+                    Offset=$fits.Where{$_.Name -eq 'OFFSET'}.value
+                    ImageType=$fits.Where{$_.Name -eq 'IMAGETYP'}.value|%{if($_){$_.Trim("'")}}
+                    CCDTemp=$fits.Where{$_.Name -eq 'CCD-TEMP'}.value
+                    SetTemp=$fits.Where{$_.Name -eq 'SET-TEMP'}.value
+                    FocalLength=$fits.Where{$_.Name -eq 'FOCALLEN'}.value
+                    FocalRatio=$fits.Where{$_.Name -eq 'FOCRATIO'}.value
+                    ObsDate=$obsDate
+                    ObsDateMinus12hr=$obsDateMinus12hr
+                    LocalDate=$fits.Where{$_.Name -eq 'DATE-LOC'}.value
+                    SSWeight=$fits.Where{$_.Name -eq 'SSWEIGHT'}.value
+                    Pedestal=$fits.Where{$_.Name -eq 'PEDESTAL'}.value
+                    History=$fits.Where{$_.Name -eq 'HISTORY'}.comment
+                    Path=$Path
+                }
+                if($Cache)
+                {
+                    $Cache.Add($Path.FullName,$result)
+                }
+                Write-Output $result
+            }
+            catch [System.Xml.XmlException]{
+                Write-Warning ("An error occured reading the file "+($Path.FullName))
+                Write-Verbose $_.Exception.ToString()
+            }
+            finally
+            {
+                $reader.Dispose()
+                $stream.Dispose()
+            }
         }
     }
 }
